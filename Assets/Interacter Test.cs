017d9f90ca824077fd7e503c5f34e1971bc0266c
bbkbli <yuchoi1561@gmail.com>
2025-04-29 15:06:07 +0900

    Initial commit

---
+++ Assets/Interacter Test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteracterTest : MonoBehaviour
+{
+    public void HoverEnter()
+    {
+        Debug.Log("HoverLog");
+    }
+
+    public void HoverExit()
+    {
+        Debug.Log("HoverExit Log");
+    }
+    public void SelectEnter()
+    {
+        Debug.Log("SelectEnter Log");
+    }
+    public void SelectExit()
+    {
+        Debug.Log("SelectExit Log");
+    }
+
+}
