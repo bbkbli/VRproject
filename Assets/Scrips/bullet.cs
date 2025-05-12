3ad3721ca196390be567e52a7492aa4a2b921d3b
bbkbli <yuchoi1561@gmail.com>
2025-05-13 04:29:43 +0900

    0512
    Gun

---
+++ Assets/Scrips/bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+}
